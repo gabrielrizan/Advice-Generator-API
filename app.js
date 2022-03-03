46d85906e27313dde37fd0cd316aa229996b7f98
gabrielrizan <100442848+gabrielrizan@users.noreply.github.com>
2022-03-03 20:43:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const fetchAdvice = async () => {
+  const res = await fetch('	https://api.adviceslip.com/advice');
+  const data = await res.json();
+
+  console.log(data);
+
+  document.getElementById('adviceid').innerHTML = `Advice #${data.slip.id}`;
+  document.getElementById('advice').innerHTML = `" ${data.slip.advice} "`;
+};
